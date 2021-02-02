e4d5bfc458f4dac616df7df55f4da46d1f6b6b57
joneikel <joneikel2010@gmail.com>
2021-02-02 17:52:58 -0400

    fix: renombramiento de campo municipio

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export function useAxios() {
     return axios.create({
-        baseURL: "http://api-actividades.guarico.gob.ve"
+        baseURL: "http://localhost:3002"
     })
 }

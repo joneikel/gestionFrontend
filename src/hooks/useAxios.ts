03a4beee106ad691fa9807441a5a31c3f0edeaca
joneikel <joneikel2010@gmail.com>
2021-02-04 09:41:40 -0400

    ruta

--- src/hooks/useAxios.ts
+++ src/hooks/useAxios.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 /*http://api-actividades.guarico.gob.ve*/
 export function useAxios() {
     return axios.create({
-        baseURL: "http://localhost:3003"
+        baseURL: "http://192.168.10.4:3300"
     })
 }

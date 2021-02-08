ffda686e21fd2421e6f8f918138d42faa2d3a8c0
aurelio <aureliogg911@.com>
2021-02-08 15:13:25 -0400

    quitarfiltros

--- src/pages/ActivityPage/index.tsx
+++ src/pages/ActivityPage/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Row, Select, Space, Tag, Form, Empty  } from 'antd';
+import { Button, Card, Col, Row, Select, Space, Tag, Form, Empty } from 'antd';
 import { AxiosInstance } from 'axios';
 import React, { useEffect, useState } from 'react';
 import MainTable from '../../components/tables/MainTable';
@@ -8,9 +8,7 @@ import ActivityCard from './components/ActivityCard';
 import MunicipiosSelect from './components/MunicipioSelect';
 import ParroquiaSelect from './components/ParroquiaSelect';
 import { useHistory } from 'react-router-dom';
-import { AnyARecord } from 'dns';
 import { useForm } from 'antd/lib/form/Form';
-import InstitutionsSelect from './components/InstitutionSelect';
 
 const ActivityPage = ({ projectId }: { projectId?: string }) => {
 
@@ -23,7 +21,7 @@ const ActivityPage = ({ projectId }: { projectId?: string }) => {
 
   const [loading, setLoading] = useState(false);
   const [activities, setActivities] = useState<Activity[] | undefined>();
-  const [parentInstitution, setParentInstitution] = useState<string| undefined>();
+  const [parentInstitution, setParentInstitution] = useState<string | undefined>();
   const [filteredActivities, setFilteredActivities] = useState<Activity[] | undefined>();
 
   useEffect(() => {
@@ -71,111 +69,16 @@ const ActivityPage = ({ projectId }: { projectId?: string }) => {
 
   return (
     <>
-      <Row gutter={[25, 10]} >
-        <Card className="filter-card" title="Filtros" >
-          <Form form={form}>
-            <Row gutter={[25, 10]} >
-
-            {/*   <Col span={4}>
-                    <Form.Item
-                        hasFeedback
-                        name="parentId"
-                        rules={[
-                        {
-                            required: true,
-                            message: "Debes seleccionar secretaría ejecutiva.",
-                        },
-                        ]}>
-                        <InstitutionsSelect onlyParent onChange={setParentInstitution} />
-                    </Form.Item>
-              </Col>
-
-              <Col span={4}>
-                    <Form.Item
-                    hasFeedback
-                    name="institution"
-                    rules={[
-                    {
-                        required: true,
-                        message: "Debes seleccionar una secretaría.",
-                    },
-                    ]}
-                    >
-                    <InstitutionsSelect
-                    parentId={parentInstitution}
-                    onChange={setParentInstitution}
-                    />
-                    </Form.Item>
-              </Col> */}
-
-              <Col span={7}>
-                <Form.Item name="municipio">
-                  <MunicipiosSelect onChange={(v: any) => {
-                    setMunicipio(v);
-                    let ActvitiesToShow = activities?.filter(x => x.municipio.id === v);
-                    setFilteredActivities(ActvitiesToShow);
-                  }} />
-                </Form.Item>
-              </Col>
-
-              <Col span={7}>
-                <Form.Item name="parroquia">
-                  <ParroquiaSelect municipio_id={municipio} onChange={(v: any) => {
-                    let ActvitiesToShow = activities?.filter(x => x.parroquia.id === v);
-                    setFilteredActivities(ActvitiesToShow);
-                  }} />
-                </Form.Item>
-              </Col>
-
-              <Col span={7}>
-                <Form.Item name="gobernador">
-                  <Select placeholder="¿Asistio el Gobernador?" onChange={(v: any) => {
-                    let gobernor: any;
-                    if (v === "SI") {
-                      gobernor = true;
-                    } else {
-                      gobernor = false;
-                    }
-                    let ActvitiesToShow = activities?.filter(x => x.gobernador === gobernor);
-                    setFilteredActivities(ActvitiesToShow);
-                  }}>
-                    <Select.Option value={"SI"}>Si</Select.Option>
-                    <Select.Option value={"NO"}>No</Select.Option>
-                  </Select>
-                </Form.Item>
-              </Col>
-
-              <Col span={2}>
-                <Form.Item>
-                  <Button onClick={() => {form.resetFields(); setFilteredActivities(activities);}} size='large' type='primary' >
-                    Descartar filtros
-              </Button>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-        </Card>
-
-        <Col span={24}>
-          {filteredActivities?.map((activity, i) => (
-            <Col span={4.8}>
-              <ActivityCard i={i} key={activity.id}
-                activity={activity} />
-            </Col>
-          ))}
-
-          {filteredActivities?.length === 0 && <Col span={4.8}>
-            <Empty 
-            description="No se encontró ninguna actividad."
-            />
-          </Col>}
-        </Col>
-
+      <Row gutter={[10, 100]}>
+        {filteredActivities?.map((activity, i) => (
+          <Col span={4}>
+            <ActivityCard i={i}
+              key={activity.id}
+              activity={activity} />
+          </Col>
+        ))}
       </Row>
     </>
-
-    
-
   )
 }
 

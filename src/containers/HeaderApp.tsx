29ae39b4a2afe74611c76d1bd4dc5cf8eb8c82d7
= <=>
2021-03-15 11:23:10 -0400

    ajuste en header

--- src/containers/HeaderApp.tsx
+++ src/containers/HeaderApp.tsx
@@ -3,39 +3,35 @@ import UserContainer from "../unstated/UserContainer";
 const { Header } = Layout;
 
 const HeaderApp = () => {
-
   const userState = UserContainer.useContainer();
 
   return (
     <Header className="header">
-      <Row align="bottom" justify="start" style={{ width: "100%" }}>
-        <Col span={23} style={{ justifyContent: "start" }}>
-          <h1 className="app-title">
-            Gestión de proyectos y actividades
-          </h1>
-        </Col>
-        <Col span={1} >
+      <div style={{display: 'flex'}}>
+        <h1 className="app-title">titulo</h1>
+        <span style={{flex: '1 1'}}></span>
+        <div style={{lineHeight: '56px'}}>
           <Dropdown placement="bottomCenter" overlay={<AvatarItems />}>
             <Avatar size={40}>{userState.user?.name.toUpperCase()} </Avatar>
           </Dropdown>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </Header>
   );
 };
 
 export default HeaderApp;
 
-
 const AvatarItems = () => {
   const userState = UserContainer.useContainer();
 
   return (
     <Menu>
-      <Menu.Item >
-        <Button type="link" onClick={() => userState.logout()}>Cerrar sesión</Button>
+      <Menu.Item>
+        <Button type="link" onClick={() => userState.logout()}>
+          Cerrar sesión
+        </Button>
       </Menu.Item>
     </Menu>
-  )
-
-}
+  );
+};

96c9d1f3f92b351e86712ded8b279ed1618901c4
= <=>
2021-03-25 15:38:45 -0400

    poa

--- src/pages/MapPage/MunicipalityInfo.tsx
+++ src/pages/MapPage/MunicipalityInfo.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Col, Divider, Row, Space, Spin, Tooltip, Typography } from "antd";
+import { Button, Card, Col, Divider, Row, Spin, Typography } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
 import { AxiosInstance } from "axios";
 import { useEffect, useState } from "react";
@@ -9,30 +9,38 @@ import { getIconByAreaCode } from "../../helpers/icons";
 const MunicipalityInfo = ({
   isOpen,
   onClose,
-  municipalityCode
+  municipalityCode,
 }: {
   isOpen: boolean;
   onClose: Function;
   municipalityCode?: string;
 }) => {
-
   const axios = useAxios();
   const [municipio, setMunicipio] = useState<Municipio | undefined>();
-  const [count, setCount] = useState<any[]>([]);
+  const [counters, setCounters] = useState<any[]>([]);
   const [loading, setLoading] = useState(false);
+
+  const institutionCodes = [
+    "E001",
+    "E002",
+    "E003",
+    "E004",
+    "E005",
+    "E006",
+    "E008",
+  ];
 
   useEffect(() => {
     if (municipalityCode) {
       setLoading(true);
-      activityCount(municipalityCode, axios)
-        .then(resp => {
-          setMunicipio(resp.municipio);
-          setCount(resp.count);
-        })
-        .catch(e => console.log(e))
-        .finally(() => setLoading(false));
+      {
+        countPPA(institutionCodes, municipalityCode, axios)
+          .then((counters) => setCounters(counters))
+          .catch((err) => console.log(err))
+          .finally(() => setLoading(false));
+      }
     }
-  }, [municipalityCode])
+  }, [municipalityCode]);
 
   return (
     <div
@@ -45,58 +53,86 @@ const MunicipalityInfo = ({
         height: "100%",
         zIndex: 500,
         backgroundColor: "#fff",
-        overflowY: 'scroll'
+        overflowY: "scroll",
       }}
     >
-      {loading ? <Spin tip="cargando" /> : <Row>
-        <Col span={22}>
-          <h2>{municipio?.name}</h2>
-        </Col>
-        <Col span={2}>
-          <Button type="link" icon={<CloseOutlined />} onClick={() => {
-            setCount([]);
-            setMunicipio(undefined);
-            onClose();
-          }} />
-        </Col>
-        <Divider />
-        {count.length > 0 && < Col span={24}>
-          {count.map((c: any, i: number) => <Tooltip title={c.area_name}>
-            <ActivityCounterWithIcon key={i} count={c} />
-          </Tooltip>)}
-        </Col>}
-      </Row>}
-    </div >
+      {loading ? (
+        <Spin tip="cargando" />
+      ) : (
+        <Row>
+          <Col span={22}>
+            <h2>{municipio?.name}</h2>
+          </Col>
+          <Col span={2}>
+            <Button
+              type="link"
+              icon={<CloseOutlined />}
+              onClick={() => {
+                setCounters([]);
+                setMunicipio(undefined);
+                onClose();
+              }}
+            />
+          </Col>
+          <Divider />
+          {counters.length > 0 &&
+            counters.map((c: any, i: number) => (
+              <Col span={24}>
+                <ActivityCounterWithIcon key={i} count={c.data} />
+              </Col>
+            ))}
+        </Row>
+      )}
+    </div>
   );
 };
 
-const ActivityCounterWithIcon = ({ count }: {
+const ActivityCounterWithIcon = ({
+  count,
+}: {
   count: {
-    activity_count: number
-    parent?: Institution
-    parent_id: string
-  }
+    programs: number;
+    projects: number;
+    activities: number;
+    institution: Institution;
+  };
 }) => {
   return (
     <Card>
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <div  className="map-card-icon" >{getIconByAreaCode(count.parent?.code)}</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
         <div>
-          <Typography.Title>{count.activity_count}</Typography.Title>
+          <Typography.Text>Programa: {count.programs}</Typography.Text>
+          <br />
+          <Typography.Text>Proyecto: {count.projects}</Typography.Text>
+          <br />
+          <Typography.Text>Act.: {count.activities}</Typography.Text>
         </div>
       </div>
-      <p style={{ backgroundColor: '#ccc', borderRadius: 4, padding: 2 }}>{count.parent?.name}</p>
+      <div className="map-card-icon">
+        {getIconByAreaCode(count.institution.code)}
+        <p style={{ backgroundColor: "#ccc", borderRadius: 4, padding: 2 }}>
+          {count.institution.name}
+        </p>
+      </div>
     </Card>
-  )
-}
+  );
+};
 
 export default MunicipalityInfo;
 
-async function activityCount(municipio_code: string, axios: AxiosInstance) {
-  const response = await axios.get('activity-count-by-municipio', {
-    params: {
-      municipio_code
-    }
-  });
-  return response.data;
+async function countPPA(
+  institutionIds: string[],
+  municipioId: string,
+  axios: AxiosInstance
+) {
+  const requests = institutionIds.map((institutionId: string) =>
+    axios.get(`count-activities/${institutionId}/${municipioId}`)
+  );
+  return Promise.all(requests);
 }
